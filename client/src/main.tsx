2fbbc00aec5f56ffa4e0f299a7847ff8a2fc963c
rayct <rayct@icloud.com>
2022-10-14 02:24:14 +0100

    first commit

---
+++ client/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// reportWebVitals(console.log);
+// function reportWebVitals(log: (...data: any[]) => void) {
+//   throw new Error('Function not implemented.');
+// }
